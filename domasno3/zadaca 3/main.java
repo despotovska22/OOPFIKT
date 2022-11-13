54b8943492ac469b0bd80a6d1b1840c8311f220a
Anastasija Despotovska <116914572+despotovska22@users.noreply.github.com>
2022-11-13 15:38:15 +0100

    Add files via upload

---
+++ domasno3/zadaca 3/main.java
@@ -0,0 +1,14 @@
+package domasno3zadaca3;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        PC pc1 = new PC();
+		
+		pc1.prvMetod(4,500);
+
+
+	}
+
+}
---
+++ domasno3/zadaca 4/main.java
@@ -0,0 +1,18 @@
+package domasno3zadaca4;
+
+public class Brojach {
+
+	public static void main(String[] args) {}
+		// TODO Auto-generated method stub
+		public int brojach;
+		
+		public void zgolemi () {
+			this.brojach++;
+		}
+		
+		public void reset () {
+			this.brojach = 0;
+	
+	}
+
+}

eeb69f088c0e799e7f660f477589ec1aa32a20d5
Anastasija Despotovska <116914572+despotovska22@users.noreply.github.com>
2022-11-13 15:37:39 +0100

    Add files via upload

---
+++ OOPFIKTZADACI.VEZBI2/domasno3/zadaca 1/main.java
@@ -0,0 +1,13 @@
+package domasno3zadaca1;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		Avtomobil av=new Avtomobil("Ford","BMW","Opel",248569, 2002, 
+				"BT-000-AA");
+				av.pMetod();
+				System.out.println(av.vMetod());
+				
+	}
+
+}
---
+++ OOPFIKTZADACI.VEZBI2/domasno3/zadaca 1/package domasno3zadaca1-avtomobil;.java
@@ -0,0 +1,36 @@
+package domasno3zadaca1;
+
+public class Avtomobil {
+
+	public static void main(String[] args) {}
+		// TODO Auto-generated method stub
+		public String marka;
+		public String model;
+		public String boja;
+		public double pominatiKm;
+		public int godinaNaProizvodstvo;
+		public String registracija;
+		
+		public Avtomobil(String marka,String model, String boja, double pominatiKm,
+		int godinaNaProizvodstvo, String registracija) {
+			
+			this.marka=marka;
+			this.model=model;
+			this.boja=boja;
+			this.pominatiKm=pominatiKm;
+			
+			this.godinaNaProizvodstvo=godinaNaProizvodstvo;
+			this.registracija=registracija;
+			
+		}
+		public void pMetod() {
+			System.out.println(this.marka+ ", "+this.model+", "+this.boja);
+			
+		}
+		public double vMetod() {
+			double pKm;
+			pKm=this.pominatiKm+1520.25;
+			return pKm;
+	}
+
+}
